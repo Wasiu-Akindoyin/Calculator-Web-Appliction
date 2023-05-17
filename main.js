eb758d8a753d3aeb0766c5174b7efd3fe886a964
Wasiu-Akindoyin <wasiuwazzy@gmail.com>
2023-05-17 03:29:44 +0100

    Updated

--- main.js
+++ main.js
@@ -10,29 +10,25 @@ let num2El = document.getElementById("num2-el").textContent = num2;
 let answerEl = document.getElementById("answer-el");
 
 //ARROW FUNCTIONS LOGIC INITIALIZATION STARTS HERE FOR THE ADDITION FUNCTION.
-const add = () =>{
-	console.log("Clicked");
+const add = () => {
 	let result = num1 + num2;
 	answerEl.textContent = "Answer: " + result;
 }
 
 //ARROW FUNCTIONS LOGIC INITIALIZATION STARTS HERE FOR THE SUBTRACTION FUNCTION.
-const subtract = () =>{
-	console.log("Clicked");
+const subtract = () => {
 	let result = num1 - num2;
 	answerEl.textContent = "Answer: " + result;
 }
 
 //ARROW FUNCTIONS LOGIC INITIALIZATION STARTS HERE FOR THE DIVISION FUNCTION.
-const divide = () =>{
-	console.log("Clicked");
+const divide = () => {
 	let result = num1 / num2;
 	answerEl.textContent = "Answer: " + result;
 }
 
 //ARROW FUNCTIONS LOGIC INITIALIZATION STARTS HERE FOR THE MULTIPLICATION FUNCTION.
-const multiply = () =>{
-	console.log("Clicked");
+const multiply = () => {
 	let result = num1 * num2;
 	answerEl.textContent = "Answer: " + result;
 }
